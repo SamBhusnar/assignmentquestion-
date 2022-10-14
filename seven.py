80a1b317f14f77cfa4f19a1013311a9badd67713
sam <samadhanbhusnar21@gmail.com>
2022-10-14 14:49:43 +0530

    samadhan bhusnar commit now

--- seven.py
+++ seven.py
@@ -1,1 +1,10 @@
-y=int(input("Enter any number\n"))
+get = int(input("Enter any number\n"))
+let = get + 1
+save = 0
+for s in range(1, let):
+    if (get % s == 0):
+        save = save + 1
+if (save == 2):
+    print("Entered number is prime ", get)
+else:
+    print("Entered number is not prime ", get)
